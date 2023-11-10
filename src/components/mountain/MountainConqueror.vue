f124f024f234bb9f3bae9efff2b22dfa3475abc6
정준원 <junwon1131@naver.com>
2023-11-10 17:54:20 +0900

    add mtnconquer

---
+++ src/components/mountain/MountainConqueror.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="card bg-info text-dark rounded-pill d-flex justify-content-around">
+    <div class="card-body flex-shrink-0">
+      <div class="flex-shrink-0">
+        <img src="..." alt="..." class="rounded-circle" />
+      </div>
+      <div class="flex-grow-1 ms-3">
+        <h6>이 산의 지배자</h6>
+        <h5>Zahi</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
