ca28f1e11a56f94d425fd23245e0460c47754d83
unistuj <115923641+unistuj@users.noreply.github.com>
2023-11-16 17:53:22 +0900

    fix:router

--- src/router/index.js
+++ src/router/index.js
@@ -2,87 +2,91 @@ import { createRouter, createWebHistory } from 'vue-router';
 // import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => import('../views/Main.vue'),
-        },
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Main.vue'),
+    },
 
-        {
-            path: '/modify',
-            name: 'modify',
+    {
+      path: '/modify',
+      name: 'modify',
 
-            component: () => import('@/views/ModifyView.vue'),
-        },
+      component: () => import('@/views/ModifyView.vue'),
+    },
 
-        {
-            path: '/conqueredmountain',
-            name: 'conqueredmountain',
+    {
+      path: '/conqueredmountain',
+      name: 'conqueredmountain',
 
-            component: () => import('@/views/ConqueredMountain.vue'),
-        },
+      component: () => import('@/views/ConqueredMountain.vue'),
+    },
 
-        {
-            path: '/board',
-            name: 'board',
+    {
+      path: '/board',
+      name: 'board',
 
-            component: () => import('../views/Board.vue'),
-        },
-        {
-            path: '/plan',
-            name: 'plan',
+      component: () => import('../views/Board.vue'),
+    },
+    {
+      path: '/plan',
+      name: 'plan',
 
-            component: () => import('../views/Plan.vue'),
-        },
-        {
-            path: '/mypage',
-            name: 'mypage',
+      component: () => import('../views/Plan.vue'),
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
 
-            component: () => import('../views/MyPage.vue'),
-        },
-        {
-            path: '/boardwrite',
-            name: 'boardwrite',
+      component: () => import('../views/MyPage.vue'),
+    },
+    {
+      path: '/boardwrite',
+      name: 'boardwrite',
 
-            component: () => import('@/views/BoardWrite.vue'),
-        },
-        {
-            path: '/login',
-            name: 'login',
+      component: () => import('@/views/BoardWrite.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
 
-            component: () => import('../views/Login.vue'),
-        },
-        {
-            path: '/register',
-            name: 'register',
+      component: () => import('../views/Login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
 
-            component: () => import('../views/Regist.vue'),
-        },
-        {
-            path: '/search',
-            name: 'search',
+      component: () => import('../views/Regist.vue'),
+    },
+    {
+      path: '/search',
+      name: 'search',
 
-            component: () => import('../views/Search.vue'),
-        },
+      component: () => import('../views/Search.vue'),
+    },
 
-        {
-            path: '/login',
-            name: 'login',
+    {
+      path: '/login',
+      name: 'login',
 
-            component: () => import('../views/Login.vue'),
-        },
+      component: () => import('../views/Login.vue'),
+    },
 
-        {
-            path: '/mountainview',
-            name: 'mountainview',
+    {
+      path: '/mountainview',
+      name: 'mountainview',
 
-            component: () => import('@/views/MountainView.vue'),
-        },
+      component: () => import('@/views/MountainView.vue'),
+    },
+    {
+      path: '/boardview',
+      name: 'boardview',
 
-         
-    ],
+      component: () => import('@/views/BoardView.vue'),
+    },
+  ],
 });
 
 export default router;
