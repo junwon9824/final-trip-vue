bc5b28ca3b182a705c4ecfc3b6afeffd5d40e2b0
unistuj <115923641+unistuj@users.noreply.github.com>
2023-11-22 17:05:20 +0900

    fix: board router

--- src/components/Main/MainCard.vue
+++ src/components/Main/MainCard.vue
@@ -1,33 +1,31 @@
 <script setup>
-import { ref, onMounted, onUpdated, onBeforeMount } from "vue";
-import { listSido, listGugun, AddconqueredMountain } from "@/api/mountain";
+import { ref, onMounted, onUpdated, onBeforeMount } from 'vue';
+import { listSido, listGugun, AddconqueredMountain } from '@/api/mountain';
 import { useRouter } from 'vue-router';
 
 const props = defineProps({
   MainCard: Object,
 });
 
-const tmpImgSrc = ref("");
+const tmpImgSrc = ref('');
 
 const router = useRouter();
 
 const navigateToMountainView = () => {
   router.push({
-    name: "MountainDetail",
+    name: 'MountainDetail',
     params: { id: props.MainCard.mntilistno },
   });
 };
 
 onBeforeMount(() => {
   tmpImgSrc.value = props.MainCard.mntiimg;
-  console.log("Image source after mounted:", tmpImgSrc.value);
+  console.log('Image source after mounted:', tmpImgSrc.value);
 });
-
 </script>
 
 <template>
-  <div class="card shadow-lg m-4" style="width: 600px">
-    <!-- <img class="card-img-top" src="../../assets/mountain_car.png" alt="Card image" /> -->
+  <div class="card shadow-lg m-4" style="width: 450px">
     <img
       class="card-img-top"
       :src="tmpImgSrc"
@@ -35,16 +33,11 @@ onBeforeMount(() => {
       style="height: 300px; width: 450px"
     />
 
-    <div
-      class="card-img-overlay d-flex justify-content-between align-items-center flex-column"
-    >
+    <div class="card-img-overlay d-flex justify-content-between align-items-center flex-column">
       <p class="card-title text-light fs-2 fw-bold">{{ MainCard.mntiname }}</p>
 
       <!-- <RouterLink to="/mountainview"> -->
-      <button
-        @click="navigateToMountainView"
-        class="btn btn-light btn-lg rounded-pill px-4 py-2"
-      >
+      <button @click="navigateToMountainView" class="btn btn-light btn-lg rounded-pill px-4 py-2">
         둘러보기<i class="bi bi-arrow-right"></i>
       </button>
       <!-- </RouterLink> -->
