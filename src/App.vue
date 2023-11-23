9213a3364c45257feb1054d701e620821f229eae
jeonwon <wjssoqls@gachon.ac.kr>
2023-11-23 21:07:59 +0900

    fix: conflict

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <!-- <TheHeader :isLoggedIn="isLogin"></TheHeader> -->
-  <TheHeader></TheHeader>
-  <!-- <router-view :isLoggedIn="isLogin"></router-view> -->
-  <router-view></router-view>
-  <TheFooter></TheFooter>
+    <!-- <TheHeader :isLoggedIn="isLogin"></TheHeader> -->
+    <TheHeader></TheHeader>
+    <!-- <router-view :isLoggedIn="isLogin"></router-view> -->
+    <router-view></router-view>
+    <TheFooter></TheFooter>
 </template>
 
 <style scoped></style>
--- src/components/wishlist/MyWishList.vue
+++ src/components/wishlist/MyWishList.vue
@@ -2,51 +2,54 @@
 import { defineProps, ref } from 'vue';
 import { deleteWishList } from '@/api/wishlist';
 defineProps({ wishlist: Object });
+import { storeToRefs } from 'pinia';
+import { useMemberStore } from '@/stores/member';
 const emit = defineEmits(['getWishLists']);
+
 const prefix = 'https://www.forest.go.kr/images/data/down/mountain';
 
+const { userInfo } = storeToRefs(useMemberStore());
+
 const wish = ref({
-  userId: '',
-  mntilistno: '',
+    userId: '',
+    mntilistno: '',
 });
 
 const deleteWishListItem = (mntilistno) => {
-  wish.value.userId = sessionStorage.userId;
-  wish.value.mntilistno = mntilistno;
+    wish.value.userId = userInfo.value.userId;
+    wish.value.mntilistno = mntilistno;
 
-  deleteWishList(
-    wish.value,
-    ({ data }) => {
-      console.log(data);
-      emit('getWishLists');
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+    deleteWishList(
+        wish.value,
+        ({ data }) => {
+            console.log(data);
+            emit('getWishLists');
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
 };
 </script>
 <template>
-  <div class="card mx-2 rounded-5 shadow" style="width: 200px">
-    <img
-      class="card-img-top px-2 pt-2 rounded-5"
-      style="height: 150px"
-      :src="`${prefix}/${wishlist.mntiimg}`"
-      alt="Card image"
-    />
-    <div class="card-body m-0 px-2">
-      <p class="fs-5 fw-bold m-0 p-0">{{ wishlist.mntiname }}</p>
-      <p class="fs-6 m-0 p-0">
-        <i class="bi bi-geo-alt-fill text-danger"></i
-        >{{ wishlist.mntiadd.substring(0, wishlist.mntiadd.length - 7) }}
-      </p>
+    <div class="card mx-2 rounded-5 shadow" style="width: 200px">
+        <img
+            class="card-img-top px-2 pt-2 rounded-5"
+            style="height: 150px"
+            :src="`${prefix}/${wishlist.mntiimg}`"
+            alt="Card image"
+        />
+        <div class="card-body m-0 px-2">
+            <p class="fs-5 fw-bold m-0 p-0">{{ wishlist.mntiname }}</p>
+            <p class="fs-6 m-0 p-0">
+                <i class="bi bi-geo-alt-fill text-danger"></i
+                >{{ wishlist.mntiadd.substring(0, wishlist.mntiadd.length - 7) }}
+            </p>
+        </div>
+        <div class="card-footer m-0 p-0">
+            <button class="btn rounded-5 p-0 ms-4" @click="deleteWishListItem(wishlist.mntilistno)">삭제</button>
+        </div>
     </div>
-    <div class="card-footer m-0 p-0">
-      <button class="btn rounded-5 p-0 ms-4" @click="deleteWishListItem(wishlist.mntilistno)">
-        삭제
-      </button>
-    </div>
-  </div>
 </template>
 
 <style scoped></style>
