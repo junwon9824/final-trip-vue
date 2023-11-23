0fea58a480aa9e662f3ddaa19c10123fbb628301
정준원 <junwon1131@naver.com>
2023-11-23 22:38:30 +0900

    writecomment fixed

--- src/stores/member.js
+++ src/stores/member.js
@@ -1,12 +1,12 @@
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { defineStore } from 'pinia';
-import { jwtDecode } from 'jwt-decode';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { defineStore } from "pinia";
+import { jwtDecode } from "jwt-decode";
 
-import { login, findById, tokenRegeneration, logout } from '@/api/members';
-import { httpStatusCode } from '@/util/http-status';
+import { login, findById, tokenRegeneration, logout } from "@/api/members";
+import { httpStatusCode } from "@/util/http-status";
 
-export const useMemberStore = defineStore('memberStore', () => {
+export const useMemberStore = defineStore("memberStore", () => {
   const router = useRouter();
 
   const isLogin = ref(false);
@@ -18,15 +18,18 @@ export const useMemberStore = defineStore('memberStore', () => {
     await login(
       loginUser,
       (response) => {
+        console.log("llllllllllllll");
+
         if (response.status === httpStatusCode.CREATE) {
+          console.log("llllllllllllll");
           let { data } = response;
-          let accessToken = data['access-token'];
-          let refreshToken = data['refresh-token'];
+          let accessToken = data["access-token"];
+          let refreshToken = data["refresh-token"];
           isLogin.value = true;
           isLoginError.value = false;
           isValidToken.value = true;
-          sessionStorage.setItem('accessToken', accessToken);
-          sessionStorage.setItem('refreshToken', refreshToken);
+          sessionStorage.setItem("accessToken", accessToken);
+          sessionStorage.setItem("refreshToken", refreshToken);
         } else {
           isLogin.value = false;
           isLoginError.value = true;
@@ -47,12 +50,12 @@ export const useMemberStore = defineStore('memberStore', () => {
         if (response.status === httpStatusCode.OK) {
           userInfo.value = response.data.userInfo;
         } else {
-          console.log('유저 정보 없음!!!!');
+          console.log("유저 정보 없음!!!!");
         }
       },
       async (error) => {
         console.error(
-          'getUserInfo() error code [토큰 만료되어 사용 불가능.] ::: ',
+          "getUserInfo() error code [토큰 만료되어 사용 불가능.] ::: ",
           error.response.status
         );
         isValidToken.value = false;
@@ -67,8 +70,8 @@ export const useMemberStore = defineStore('memberStore', () => {
       JSON.stringify(userInfo.value),
       (response) => {
         if (response.status === httpStatusCode.CREATE) {
-          let accessToken = response.data['access-token'];
-          sessionStorage.setItem('accessToken', accessToken);
+          let accessToken = response.data["access-token"];
+          sessionStorage.setItem("accessToken", accessToken);
           isValidToken.value = true;
         }
       },
@@ -80,15 +83,15 @@ export const useMemberStore = defineStore('memberStore', () => {
             userInfo.value.userid,
             (response) => {
               if (response.status === httpStatusCode.OK) {
-                console.log('리프레시 토큰 제거 성공');
+                console.log("리프레시 토큰 제거 성공");
               } else {
-                console.log('리프레시 토큰 제거 실패');
+                console.log("리프레시 토큰 제거 실패");
               }
-              alert('RefreshToken 기간 만료!!! 다시 로그인해 주세요.');
+              alert("RefreshToken 기간 만료!!! 다시 로그인해 주세요.");
               isLogin.value = false;
               userInfo.value = null;
               isValidToken.value = false;
-              router.push({ name: 'user-login' });
+              router.push({ name: "user-login" });
             },
             (error) => {
               console.error(error);
@@ -110,7 +113,7 @@ export const useMemberStore = defineStore('memberStore', () => {
           userInfo.value = null;
           isValidToken.value = false;
         } else {
-          console.error('유저 정보 없음!!!!');
+          console.error("유저 정보 없음!!!!");
         }
       },
       (error) => {
